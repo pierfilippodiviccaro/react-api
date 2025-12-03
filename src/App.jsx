5d64f201395f9d43a392e5703c0672a3a42bab66
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-12-03 17:01:28 +0100

    feat: inizio esercizio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css'
-
+import Card from './components/Card'
 function App() {
  const [attori,setAttori] = useState([]);
  const [totale, setTotale] = useState(null);
@@ -11,17 +11,26 @@ const [totalePagine, setTotalePagine] =useState(null);
 
   useEffect(()=>{
     fetchAttori();
-  },[page])
+  },[pagina])
 
   function fetchAttori(){
     setCaricamento(true);
     axios
-    .get
+    .get('https://lanciweb.github.io/demo/api/actors/','https://lanciweb.github.io/demo/api/actresses/')
+    .then((resp)=>{
+      setAttori(resp.data.results)
+      setTotale(resp.data.info.count)
+      setTotalePagine(resp.data.info.pages)
+      setCaricamento(false);
+    }) 
   }
 
 return (
     <>
-      
+      <div className="container">
+        <h1>Actors</h1>
+        <p>list of actors fetched from an api</p>
+      </div>
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+export default function Card({attore}){
+    const {name, birth_year,nationality,biography, image, awards} = attore
+    return(
+        <div className="card">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={image} className="img-fluid rounded-start"/>
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p>{birth_year}</p>
+            <p>{nationality}</p>
+            <p>{biography}</p>
+            <p>i suoi premi vinti :{awards}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+)
+}
