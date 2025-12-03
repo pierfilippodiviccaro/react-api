706ac59b591abb6b6e7f959e9947eee3fedf656d
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-12-03 18:46:23 +0100

    fix: sistemato errore di typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
-import Card from './components/card'
+import Card from './components/Card'
 
 function App() {
   const [attori, setAttori] = useState([]);  
